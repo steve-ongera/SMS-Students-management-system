f648c0f42761a9fa6a8078ed7c40e2d3279e3180
steve-ongera <gadafisteve001@gmail.com>
2026-04-11 15:33:23 +0300

    made updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AuthProvider }  from './context/AuthContext';
+import { ToastProvider } from './context/ToastContext';
+import App from './App';
+import './styles/main.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </AuthProvider>
+  </StrictMode>
+);
